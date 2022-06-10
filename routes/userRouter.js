fb0411b38fd55c7247ed83f2a1650e5a00e8514e
amad2988 <uddinwork@gmail.com>
2022-06-10 11:36:19 -0400

    new commit

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const userModel = require('../models/userSchema')
 const {check, validationResult} = require('express-validator')
+const bcrypt = require('bcrypt')
 
 const router = express.Router()
 
@@ -15,6 +16,7 @@ const router = express.Router()
 router.post('/', [
     check('username', "Username is required from Middleware!").notEmpty(),
     check("email", "Please use a valid email! from middleware").isEmail(),
+    check("password", "Please enter a password").notEmpty(),
     check("password", "Please enter a password with six or more characters").isLength({min: 6})
 ],
 
@@ -28,6 +30,14 @@ async (req, res) => {
     }
 
     try{
+        const userExist = await userModel.findOneAndDelete({email: userData.email})
+        if (userExist) {
+            return res.json({msg: "User already exist!"})
+        }
+        const SALT = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(userData.password, SALT)
+        userData.password = hashedPassword
+
         const users = await userModel.create(userData)
         res.status(201).json(users)
     } catch (error) {
--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const app = express()
 const mongoConfig = require('./config/mongoConfig')
 const contactRouter = require('./routes/contactRouter')
 const userRouter = require('./routes/userRouter')
+const authRouter = require('./routes/authRouter')
 const PORT = 6000 
 
 app.use(express.json())
 app.use('/contact', contactRouter)
 app.use('/users', userRouter)
+app.use('/login', authRouter)
 
 app.get('/', (req, res) => {
     res.status(200).json({message: 'Contacts'})
